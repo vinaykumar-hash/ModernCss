aa2881c80fbf1a435c9fd219ff58a436f9b10dc7
creatersapp <vinaykumar752575@gmail.com>
2023-08-16 13:14:40 +0530

    fix navigate to mainpage

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,17 @@
+"use client"
 import './globals.css'
-import NavBar from '@/components/NavBar'
-// import HomePage from '@/components/HomePage'
-// import Category from '@/components/Category'
-// import HomePageFill from '@/components/HomePageFill'
-export const metadata = {
-  title: 'ModernCSS',
-  description: 'Modern css , custom css',
-}
+import Link from 'next/link'
+import { useEffect } from 'react'
+// export const metadata = {
+//   title: 'ModernCSS',
+//   description: 'Modern css , custom css',
+// }
 
 export default function RootLayout({children}) {
+  useEffect(()=>{
+    document.getElementById("navigate").click()
+  },[])
+  
   return (
     <html>
       <head>
@@ -19,11 +22,7 @@ export default function RootLayout({children}) {
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet"></link>
       <body>
         {children}
-        {/* <div className='w-screen h-screen flex sm:justify-between justify-normal items-start mt-4 sm:flex-row flex-col  sm:px-0 px-2'>
-          <Category/>
-          <HomePage/>
-          <HomePageFill/>
-        </div> */}
+        <Link id='navigate' href="/mainpage"></Link>
       </body>
     </html>
   )
